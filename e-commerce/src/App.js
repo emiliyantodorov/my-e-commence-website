7ccee16b759235ff51e4ca2edcff468cef2a34dd
emiliyantodorov <emiliyantodorov@abv.bg>
2022-05-29 21:07:06 +0300

    Here we copy the menu card 3 times which is wrong because we repet the code

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
+// Here we copy the menu card 3 times which is wrong because we repet the code
 
 class App extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>E Commerce</h1>
-            </div>
+            <section className='menu-categoris'>
+                <div className='container'>
+                    <h1>E Commerce</h1>
+
+                    <div className='card-menu'>
+                        <div className='card-menu__body'>
+                            <h2>HATS</h2>
+                            <h3>Shop now</h3>
+                        </div>
+                    </div>
+                    <div className='card-menu'>
+                        <div className='card-menu__body'>
+                            <h2>JACKETS</h2>
+                            <h3>Shop now</h3>
+                        </div>
+                    </div>
+                    <div className='card-menu'>
+                        <div className='card-menu__body'>
+                            <h2>SNEAKERS</h2>
+                            <h3>Shop now</h3>
+                        </div>
+                    </div>
+                </div>
+            </section>
         )
     }
 }
