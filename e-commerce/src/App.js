7cb6e70ae0fc228ea21f2b4a10b5968dc1a13808
emiliyantodorov <emiliyantodorov@abv.bg>
2022-05-29 22:05:35 +0300

    aaa

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 // Here we copy the menu card 3 times which is wrong because we repet the code
 
+const menuCardsData = [
+    { title: 'HATS', id: 1 },
+    { title: 'SNEAKERS', id: 2 },
+    { title: 'JACKETS', id: 3 },
+]
+
 class App extends React.Component {
 
     render() {
@@ -9,24 +15,19 @@ class App extends React.Component {
                 <div className='container'>
                     <h1>E Commerce</h1>
 
-                    <div className='card-menu'>
-                        <div className='card-menu__body'>
-                            <h2>HATS</h2>
-                            <h3>Shop now</h3>
-                        </div>
-                    </div>
-                    <div className='card-menu'>
-                        <div className='card-menu__body'>
-                            <h2>JACKETS</h2>
-                            <h3>Shop now</h3>
-                        </div>
-                    </div>
-                    <div className='card-menu'>
-                        <div className='card-menu__body'>
-                            <h2>SNEAKERS</h2>
-                            <h3>Shop now</h3>
-                        </div>
-                    </div>
+                    {
+                        menuCardsData.map(({ title, id }) => {
+                            return (
+                                <div key={id} className='card-menu'>
+                                    <div className='card-menu__body'>
+                                        <h2>{title}</h2>
+                                        <h3>Shop now</h3>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+
                 </div>
             </section>
         )
